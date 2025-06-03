43a5da8eec57b398d562a8b694360e5e2873d2b2
Unpa <p13163t0yss1td@me.com>
2025-06-04 06:26:16 +0900

    Initial Commit

---
+++ MemoApp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MemoApp
+//
+//  Created by YujiYamanaka on 2025/06/04.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ MemoAppTests/MemoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MemoAppTests.swift
+//  MemoAppTests
+//
+//  Created by YujiYamanaka on 2025/06/04.
+//
+
+import Testing
+@testable import MemoApp
+
+struct MemoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
